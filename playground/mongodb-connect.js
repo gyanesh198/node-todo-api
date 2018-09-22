50e5fe4789942dddc543a79a2632e50a2d2a4850
Gyanesh <gyanesh.formal@gmail.com>
2018-09-22 11:42:37 -0700

    CRUDDONE

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,22 +6,37 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   }
   console.log('Connected to mongodb');
 
-  db.collection('Todos').findOneAndDelete({completed:true}).then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
+  db.collection('Todos').insertOne({
+    _id:new ObjectId(),
+    text:'Something to do',
+    completed: 1
+  },(err,result)=>{
+    if(err){
+      return console.log(err);
+    }
+    console.log(result.ops[0]._id);
+  });
+
+  db.collection('Todos').findOneAndUpdate({
+    completed:1
+    },
+    {
+      $set:{
+        text:"Lunch Eaten",
+      },
+      $inc:{
+        completed:1
+      }
+    }
+,{returnOriginal:true}).then((res)=>console.log(res));
+
+  // db.collection('Todos').findOneAndDelete({completed:true}).then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
 
   // db.collection('Todos').find().toArray().then((docs)=>{
   //   console.log(docs);
   // }).catch((err)=>{console.log(err);});
 
-  // db.collection('Todos').insertOne({
-  //   _id:new ObjectId(),
-  //   text:'Something to do',
-  //   completed: false
-  // },(err,result)=>{
-  //   if(err){
-  //     return console.log(err);
-  //   }
-  //   console.log(result.ops[0]._id);
-  // });
+
   db.close();
 
 });
